aeb71556c2bb790d3303771b261750fe5335efd2
Daniel Yovchev <daniel3d@ymail.com>
2017-06-24 17:02:03 +0100

    Custom2 and Custom3 functions
    -Implement custom 2 and custom 3 functions
    -Increase the tournament to 100 games

--- game_agent.py
+++ game_agent.py
@@ -66,8 +66,16 @@ def custom_score_2(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
-    # TODO: finish this function!
-    raise NotImplementedError
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    w, h = game.width / 2., game.height / 2.
+    y, x = game.get_player_location(player)
+
+    return float(((h - y)**2 + (w - x)**2) + custom_score(game, player))
 
 
 def custom_score_3(game, player):
@@ -92,8 +100,13 @@ def custom_score_3(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
-    # TODO: finish this function!
-    raise NotImplementedError
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    return float(len(game.get_legal_moves(player)) + custom_score_2(game, player))
 
 
 class IsolationPlayer:
